68116b2a3627eeb33d5aa6f4b538fc04a4364b31
Dok Vanchhay <vanchhaydok@gmail.com>
2025-06-03 03:23:09 +0000

    please make ui more respone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,8 @@ export default async function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
+        {/* Updated to import Inter font as configured in tailwind.config.ts */}
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         {settings.globalHeaderScriptsEnabled && settings.globalHeaderScriptsCustomHtml && (
           <HeadScriptInjector htmlString={settings.globalHeaderScriptsCustomHtml} />
         )}
